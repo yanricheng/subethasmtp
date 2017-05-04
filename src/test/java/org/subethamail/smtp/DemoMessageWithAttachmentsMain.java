443ff513754c81bfe88a709038d786da695dd7a5
Dave Moten <davidmoten@gmail.com>
2017-05-04 12:47:05 +1000

    add Demo class to send a message to server using java mail

---
+++ src/test/java/org/subethamail/smtp/DemoMessageWithAttachmentsMain.java
@@ -0,0 +1,73 @@
+package org.subethamail.smtp;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+
+public class DemoMessageWithAttachmentsMain {
+
+    //Before running this start BasicSMTPServer.main
+    public static void main(String[] args) throws MessagingException {
+        String to = "me@gmail.com";
+        String from = "fred@gmail.com";
+        String host = "127.0.0.1";
+        Properties props = new Properties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", 25000);
+        Session session = Session.getInstance(props);
+        // Create a default MimeMessage object.
+        Message message = new MimeMessage(session);
+
+        // Set From: header field of the header.
+        message.setFrom(new InternetAddress(from));
+
+        // Set To: header field of the header.
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+        // Set Subject: header field
+        message.setSubject("Testing Subject");
+
+        // Create the message part
+        BodyPart messageBodyPart = new MimeBodyPart();
+
+        // Now set the actual message
+        messageBodyPart.setText("This is message body");
+
+        // Create a multipar message
+        Multipart multipart = new MimeMultipart();
+
+        // Set text message part
+        multipart.addBodyPart(messageBodyPart);
+
+        // Part two is attachment
+        messageBodyPart = new MimeBodyPart();
+        String attachment = "hi there it's me";
+        DataSource source = new ByteArrayDataSource(attachment.getBytes(StandardCharsets.UTF_8), "text/plain");
+        messageBodyPart.setDataHandler(new DataHandler(source));
+        messageBodyPart.setFileName("message.txt");
+        multipart.addBodyPart(messageBodyPart);
+
+        // Send the complete message parts
+        message.setContent(multipart);
+
+        // Send message
+        Transport.send(message);
+
+        System.out.println("Sent message successfully....");
+
+    }
+
+}
