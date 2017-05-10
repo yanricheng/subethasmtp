971920452bc5e7d518c384ec969e06d1582e0e60
Dave Moten <davidmoten@gmail.com>
2017-05-10 17:56:35 +1000

    add Constants test

---
+++ src/test/java/org/subethamail/smtp/internal/ConstantsTest.java
@@ -0,0 +1,14 @@
+package org.subethamail.smtp.internal;
+
+import org.junit.Test;
+
+import com.github.davidmoten.junit.Asserts;
+
+public class ConstantsTest {
+    
+    @Test
+    public void isUtilityClass() {
+        Asserts.assertIsUtilityClass(Constants.class);
+    }
+
+}
