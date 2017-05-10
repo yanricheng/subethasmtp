69f8bf24de819fdf7349920f777c464a6cdfd3f8
Dave Moten <davidmoten@gmail.com>
2017-05-11 08:10:23 +1000

    add client test with BasicSMTPServer

--- src/test/java/org/subethamail/smtp/examples/BasicSMTPServerTest.java
+++ src/test/java/org/subethamail/smtp/examples/BasicSMTPServerTest.java
@@ -1,15 +1,29 @@
 package org.subethamail.smtp.examples;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 import org.junit.Test;
+import org.subethamail.smtp.client.SMTPException;
+import org.subethamail.smtp.client.SmartClient;
 import org.subethamail.smtp.examples.BasicSMTPServer;
 import org.subethamail.smtp.server.SMTPServer;
 
 public class BasicSMTPServerTest {
 
     @Test
-    public void test() throws InterruptedException {
+    public void test() throws InterruptedException, UnknownHostException, SMTPException, IOException {
         SMTPServer server = new BasicSMTPServer().start(25000);
-        Thread.sleep(1000);
+        Thread.sleep(500);
+        SmartClient client = SmartClient.createAndConnect("localhost", 25000, "clientHost");
+        client.from("me@me.com");
+        client.to("fred@gmail.com");
+        client.dataStart();
+        byte[] bytes = "stuff".getBytes();
+        client.dataWrite(bytes, bytes.length);
+        client.dataEnd();
+        client.quit();
+        Thread.sleep(500);
         server.stop();
     }
     
