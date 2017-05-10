5002a1d04305d54c6d8051882cbf6c8ff177415e
Dave Moten <davidmoten@gmail.com>
2017-05-10 18:04:43 +1000

    improve test coverage of TextUtils and throw IllegalArgumentException instead of IllegalStateException

--- src/test/java/org/subethamail/smtp/internal/util/TextUtilsTest.java
+++ src/test/java/org/subethamail/smtp/internal/util/TextUtilsTest.java
@@ -3,14 +3,21 @@ package org.subethamail.smtp.internal.util;
 import org.junit.Test;
 import org.subethamail.smtp.internal.util.TextUtils;
 
+import com.github.davidmoten.junit.Asserts;
+
 public class TextUtilsTest {
-    
-    @Test(expected=IllegalArgumentException.class)
+
+    @Test
+    public void isUtilityClass() {
+        Asserts.assertIsUtilityClass(TextUtils.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void testGetBytesUnsupportedCharset() {
         TextUtils.getBytes("hello there", "DOES NOT EXIST");
     }
-    
-    @Test(expected=IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void testGetStringUnsupportedCharset() {
         TextUtils.getString("hello there".getBytes(), "DOES NOT EXIST");
     }
