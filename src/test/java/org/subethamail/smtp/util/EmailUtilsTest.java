e250616176bf96cd160e551ec6646e70a2b22166
Dave Moten <davidmoten@gmail.com>
2017-05-11 10:45:44 +1000

    add test coverage of EmailUtils

--- src/test/java/org/subethamail/smtp/util/EmailUtilsTest.java
+++ src/test/java/org/subethamail/smtp/util/EmailUtilsTest.java
@@ -1,5 +1,7 @@
 package org.subethamail.smtp.util;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.subethamail.smtp.internal.util.EmailUtils;
@@ -21,6 +23,16 @@ public class EmailUtilsTest {
     @Test
     public void testBlankAddressIsValid() {
         Assert.assertTrue(EmailUtils.isValidEmailAddress(""));
+    }
+    
+    @Test
+    public void testExtract() {
+        assertEquals("anyone2@anywhere.com", EmailUtils.extractEmailAddress("TO:<anyone2@anywhere.com>" ,3));
+    }
+    
+    @Test
+    public void testExtractWithSpace() {
+        assertEquals("anyone2@anywhere.com", EmailUtils.extractEmailAddress("TO:<anyone2@anywhere.com> something" ,3));
     }
     
 
