73d8a0e7854f23410cbd537b99e337b9174f691a
Dave Moten <davidmoten@gmail.com>
2017-08-10 16:42:48 +1000

    add throws RejectException to BasicMessageListener.messageArrived

--- src/main/java/org/subethamail/smtp/helper/BasicMessageListener.java
+++ src/main/java/org/subethamail/smtp/helper/BasicMessageListener.java
@@ -1,7 +1,22 @@
 package org.subethamail.smtp.helper;
 
+import org.subethamail.smtp.RejectException;
+
 public interface BasicMessageListener {
-    
-    void messageArrived(String from, String to, byte[] data);
+
+    /**
+     * Process a message that has just arrived. If you throw and you want the caller
+     * to be given the SMTP error response then throw a {@link RejectException}.
+     * 
+     * @param from
+     *            source of message
+     * @param to
+     *            destination of message
+     * @param data
+     *            message content
+     * @throws RejectException
+     *             when caller to be given an SMTP error response
+     */
+    void messageArrived(String from, String to, byte[] data) throws RejectException;
 
 }
